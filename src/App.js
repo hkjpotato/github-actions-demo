79d6905a97f824f841a4210b6d6096ed00994778
hkjpotato <hkjpotato@gmail.com>
2021-05-19 15:20:23 -0400

    deploy to gh-page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React & See with actions
+          Learn React & See with actions in gh-pages
         </a>
       </header>
     </div>
